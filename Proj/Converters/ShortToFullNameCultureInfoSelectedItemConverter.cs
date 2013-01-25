c8bc1aee9c76e953a293216db2dd789a247170ea
mrlukz <lukasz.mocarski@gmail.com>
2013-01-25 22:22:34 +0100

    Dodane konwertery pl=>polski

---
+++ Proj/Converters/ShortToFullNameCultureInfoSelectedItemConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Proj.Converters
+{
+    class ShortToFullNameCultureInfoSelectedItemConverter : IValueConverter 
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return new CultureInfo(value.ToString()).NativeName;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            CultureInfo[] allCultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
+
+            foreach (CultureInfo c in allCultures)
+            {
+                if (c.NativeName == value.ToString()) return c;
+            }
+
+            return null;
+
+        }
+    }
+}
---
+++ Proj/Converters/ShortToFullNamesCultureInfoItemsSourceConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Proj.Converters
+{
+    class ShortToFullNamesCultureInfoItemsSourceConverter : IValueConverter 
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+
+            List<string> langs = new List<string>();
+
+            foreach (var item in value as IEnumerable<CultureInfo>)
+            {
+                langs.Add(item.NativeName);
+            }
+
+            return langs;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {           
+            return value;
+        }
+    }
+}
