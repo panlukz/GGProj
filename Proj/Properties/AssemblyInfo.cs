4db2e1e68df700ca4d7cbd19b633f1cc23765e9d
mrlukz <lukasz.mocarski@gmail.com>
2013-01-26 20:39:06 +0100

    auto

--- Proj/Properties/AssemblyInfo.cs
+++ Proj/Properties/AssemblyInfo.cs
@@ -53,4 +53,4 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.2")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: NeutralResourcesLanguageAttribute("pl-PL")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
